9e49bb0774501638c4fd839a365dbb0394c3b58b
GiangVB <giangvb192@gmaiil.com>
2022-06-02 11:10:05 +0700

    UD & boostrap commit

--- src/app/book/book-delete/book-delete.component.ts
+++ src/app/book/book-delete/book-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {BookService} from '../../service/book.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-delete',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-delete.component.css']
 })
 export class BookDeleteComponent implements OnInit {
+  bookForm: FormGroup;
+  id: number;
 
-  constructor() { }
+  constructor(private bookService: BookService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+      this.getBook(this.id);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  getBook(id: number) {
+    return this.bookService.findById(id).subscribe(book => {
+      this.bookForm = new FormGroup({
+        title: new FormControl(book.title),
+        author: new FormControl(book.author),
+        description: new FormControl(book.description)
+      });
+    });
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe(() => {
+      this.router.navigate(['/book/list']);
+    }, e => console.log(e));
   }
 
 }
--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {BookService} from '../../service/book.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-book-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-detail.component.css']
 })
 export class BookDetailComponent implements OnInit {
+  bookForm: FormGroup;
+  id: number;
 
-  constructor() { }
+  constructor(private bookService: BookService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+      this.getBook(this.id);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  getBook(id: number) {
+    return this.bookService.findById(id).subscribe(book => {
+      this.bookForm = new FormGroup({
+        title: new FormControl(book.title),
+        author: new FormControl(book.author),
+        description: new FormControl(book.description)
+      });
+    });
   }
 
 }
